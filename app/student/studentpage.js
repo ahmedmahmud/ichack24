0686efef344c184bd48df691a697a203f7591122
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-02-03 23:50:26 +0000

    daisyui

---
+++ app/student/studentpage.js
@@ -0,0 +1,14 @@
+// pages/student/StudentPage.js
+import React from 'react';
+import { Heading } from 'daisyui';
+
+const StudentPage = () => {
+  return (
+    <div>
+      <Heading>Student Page</Heading>
+      {/* Add student-specific content here */}
+    </div>
+  );
+};
+
+export default StudentPage;
---
+++ app/teacher/teacherpage.js
@@ -0,0 +1,14 @@
+// pages/teacher/TeacherPage.js
+import React from 'react';
+import { Heading } from 'daisyui';
+
+const TeacherPage = () => {
+  return (
+    <div>
+      <Heading>Teacher Page</Heading>
+      {/* Add teacher-specific content here */}
+    </div>
+  );
+};
+
+export default TeacherPage;
