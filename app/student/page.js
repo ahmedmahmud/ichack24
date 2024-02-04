769aebafae95154702f4e9bc616e2097c99f230c
ahmedmahmud <ahmedmahmud12971@gmail.com>
2024-02-04 10:32:55 +0000

    style: improve student dashboard

--- app/student/page.js
+++ app/student/page.js
@@ -2,54 +2,42 @@
 'use client'
 import React from 'react';
 
- 
-
 const StudentPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="bg-gray-800 p-8 rounded-md shadow-md text-center max-w-md w-full mx-auto">
-        <h1 className="text-3xl font-semibold mb-8">Student Dashboard</h1>
-
-        {/* File Input and Submit Assignment Button */}
-        <div className="join join-vertical">
-          <input
-            type="file"
-            id="fileInput"
-            className="hidden"
-          />
-          <label
-            htmlFor="fileInput"
-            className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-md w-full inline-block"
-          >
-            Choose File
-          </label>
-          <button
-            className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md w-full inline-block"
-            onClick={()=>document.getElementById('my_modal_4').showModal()}>
-            Submit Assignment
-          </button>
-          <dialog id="my_modal_4" className="modal">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click outside to close</p>
-  </div>
-  <form method="dialog" className="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
-
-        
-
-        {/* Speech Practice Button */}
-        <a href="/student/speech_practice"  className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md w-full inline-block">
-          Speech Practice
-        </a>
-
-        {/* Feedback Page Button */}
-        <a href="/student/feedback" className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md w-full inline-block">
-          View Feedback
-        </a>
+    <div className='h-screen'>
+      <nav className='mx-4'>
+        <div className='mx-auto py-5 text-center text-4xl tracking-tighter text-white/80'>
+          <p><span className='font-bold'>Professor</span> Phil</p>
         </div>
+      </nav>
+      <div className="flex flex-col max-w-md mx-auto text-white py-8">
+        <h1 className="text-2xl font-semibold">Assignments</h1>
+        <p className='mb-8 mt-2 text-md'>
+          A list of verbal assignments set by your teacher for you to complete
+        </p>
+        <button className="card bg-base-100 shadow-xl border border-primary/40 text-left">
+          <div className="card-body p-4">
+            <h2 className="card-title">Assignment 1</h2>
+            <p>Description</p>
+          </div>
+        </button>
+        <div className="divider mt-10 mb-10">OR</div>
+        <h1 className="text-2xl font-semibold">Free practice</h1>
+        <p className='mb-8 mt-2 text-md'>
+          Practice a conversation with a virtual teacher in your own comfort
+        </p>
+        <button className='btn btn-primary'>Speech Practice</button>
+        {/* <div className="bg-gray-800 p-8 rounded-md shadow-md text-center max-w-md w-full mx-auto">
+
+        <div className="join join-vertical">
+          <a href="/student/speech_practice" className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md w-full inline-block">
+            Speech Practice
+          </a>
+          <a href="/student/feedback" className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md w-full inline-block">
+            View Feedback
+          </a>
+        </div>
+      </div> */}
       </div>
     </div>
   );
