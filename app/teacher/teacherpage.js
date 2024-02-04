dcb0bc3d6fba64f382dc0a084b5b22075ab44ee1
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-02-04 02:48:08 +0000

    teacher sekelteon structure

--- app/teacher/teacherpage.js
+++ app/teacher/teacherpage.js
@@ -1,14 +1,21 @@
-// pages/teacher/TeacherPage.js
+// components/TeacherPage.js
 import React from 'react';
-import { Heading } from 'daisyui';
 
 const TeacherPage = () => {
   return (
-    <div>
-      <Heading>Teacher Page</Heading>
-      {/* Add teacher-specific content here */}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-md shadow-md text-center">
+        <h1 className="text-3xl font-semibold mb-8">Student Dashboard</h1>
+
+        {/* Assignment Submission Button */}
+        <ul className="menu bg-base-200 w-56">
+  <li><a href="/teacher/upload_syllabus" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">Upload Syllabus</a></li>
+  <li><a href="/teacher/mark_schemes" className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-md"> Upload Mark Schemes</a></li>
+  <li><a href="/teacher/review_feedback" className="bg-green-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-md">Review Feedback</a></li>
+</ul>
+      </div>
     </div>
   );
 };
 
-export default TeacherPage;
+export default StudentPage;
